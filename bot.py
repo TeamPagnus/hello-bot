50bb73c48cd768ee950eebbd8fb381f38fea9ecd
Pablo Vannini <vanninipablo@gmail.com>
2021-06-21 21:07:17 -0300

    init of hello bot

---
+++ bot.py
@@ -0,0 +1,37 @@
+from flask import Flask, request
+import telegram
+import os
+
+# Get secrets from enviroment variable
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_URL = os.getenv("BOT_URL")
+
+bot = telegram.Bot(token=BOT_TOKEN)
+
+app = Flask(__name__)
+
+@app.route(f'/{BOT_TOKEN}')
+def process_message():
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+
+    chat_id = update.message.chat.id
+    msg_id = update.message.message_id    
+    bot.sendMessage(chat_id=chat_id, text="HELLO", reply_to_message=msg_id)
+
+    return 'ok'
+
+@app.route('/setwebhook', methods=['GET', 'POST'])
+def set_webhook():
+    s = bot.setWebhook(f'{BOT_URL}{BOT_TOKEN}')
+
+    if s:
+        return "SETUP OK"
+    else:
+        return "SETUP FAILED"
+
+@app.route('/')
+def index():
+    return '.'
+
+if __name__ == '__main__':
+    app.run(threaded=True)
